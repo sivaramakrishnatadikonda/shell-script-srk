e2de9d81a271e9f8c02049e3fa5ecf78ae3657be
sivaramakrishnatadikonda <sivaramakrishnatadikonda123@gmail.com>
2025-05-19 15:49:33 +0530

    hello world started

---
+++ 04-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1: hey PERSON2 how are you?"
+echo "$PERSON2:hello $PERSON1 i am fine.how are you doing"
+echo "$PERSON1: i am fine.what about you"
+echo "$PERSON2: i am good.today i am going to uk.if you dont mine you can also join"
+echo "$PERSON1: yeah no thanks .i have some work today"
