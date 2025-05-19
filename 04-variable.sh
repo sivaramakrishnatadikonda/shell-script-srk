bb02e7ca6bd4c228ca46b2b5543b6b0f5d23cbe1
sivaramakrishnatadikonda <sivaramakrishnatadikonda123@gmail.com>
2025-05-19 15:54:10 +0530

    hello world started

--- 04-variable.sh
+++ 04-variable.sh
@@ -2,7 +2,7 @@
 
 PERSON1=$1
 PERSON2=$2
-echo "$PERSON1: hey PERSON2 how are you?"
+echo "$PERSON1: hey $PERSON2 how are you?"
 echo "$PERSON2:hello $PERSON1 i am fine.how are you doing"
 echo "$PERSON1: i am fine.what about you"
 echo "$PERSON2: i am good.today i am going to uk.if you dont mine you can also join"
