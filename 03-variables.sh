a802337f38ecd5de0f8003f95b2c4ddbc7407238
sivaramakrishnatadikonda <sivaramakrishnatadikonda123@gmail.com>
2025-05-19 14:56:59 +0530

    hello world started

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,7 +3,7 @@
 PERSON1=siva
 PERSON2=ram
 echo "$PERSON1: hey PERSON2 how are you?"
-echo "PERSON2:hello $PERSON1 i am fine.how are you doing"
+echo "$PERSON2:hello $PERSON1 i am fine.how are you doing"
 echo "$PERSON1: i am fine.what about you"
-echo "PERSON2: i am good.today i am going to uk.if you dont mine you can also join"
+echo "$PERSON2: i am good.today i am going to uk.if you dont mine you can also join"
 echo "$PERSON1: yeah no thanks .i have some work today"
