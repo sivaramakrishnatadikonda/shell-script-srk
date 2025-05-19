e4244052c67644b58b60e89794efa5ab1c46778c
sivaramakrishnatadikonda <sivaramakrishnatadikonda123@gmail.com>
2025-05-19 19:09:19 +0530

    hello world started

--- 10-install-commands.sh
+++ 10-install-commands.sh
@@ -10,14 +10,29 @@ else
     echo "you are running with root access"
 fi
 
- dnf install mysql -y
+
+dnf list installed nginx
+
+if [ $? -ne 0]
+
+then 
+
+    echo "Nginx is not installed please installed"
+
+
+     dnf install nginx -y
 
 if [ $? -eq 0 ]
 then
-    echo "Inatalling the MYSQL ....SUCCESSFULLY"
+    echo "Inatalling the NGINX ....SUCCESSFULLY"
     #exit 0
 
 else 
-     echo "Installing MYSQL ..... FAILED"
+     echo "Installing NGINX ..... FAILED"
     exit 1
+fi
+else 
+   echo "NGINX is already installed....don't do anything"
+   exit 1
+
 fi
