aa153a8f0d51380b6a66ee99e66a9f5d43075950
sivaramakrishnatadikonda <sivaramakrishnatadikonda123@gmail.com>
2025-05-19 18:14:11 +0530

    hello world started

---
+++ 09-if-else-conditions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NUMBER=100
+if [ $NUMBER -lt 100 ]
+than 
+
+    echo "Give number $NUMBER is less than 100"
+    
+    else
+        
+         echo "Given number $NUMBER is not less than 100"
+    fi
