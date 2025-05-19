8847e7ab805c99ea39f2c1a0935db7b81093bb80
sivaramakrishnatadikonda <sivaramakrishnatadikonda123@gmail.com>
2025-05-19 14:50:45 +0530

    hello world started

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "siva: hey ramhow are you?"
+echo "ram:hello sivai am fine.how are you doing"
+echo "siva: i am fine.what about you"
+echo "ram: i am good.today i am going to uk.if you dont mine you can also join"
+echo "siva: yeah no thanks .i have some work today"
