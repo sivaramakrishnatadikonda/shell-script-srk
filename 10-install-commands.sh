ed8599056b6476b2af3df18517e6b5459ebf1eb9
sivaramakrishnatadikonda <sivaramakrishnatadikonda123@gmail.com>
2025-05-19 18:55:16 +0530

    hello world started

--- 10-install-commands.sh
+++ 10-install-commands.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
+
 USERID=$(id -u)
 
-if [ $USERID -ne 0 ]
+if [ $USERID -eq 0 ]
 then
-     echo "ERROR: code running with root access"
-     exit 1
+     echo "you are running with root access"
+     exit 0
 else
-    echo "you are running with root access"
+    echo "ERROR: code running with root access"
 fi
 
- dnf install mysqlqqwwer -y
+ dnf install mysql -y
 
 if [ $? -eq 0 ]
 then
     echo "Inatalling the MYSQL ....SUCCESSFULLY"
+    exit 0
 
 else 
      echo "Installing MYSQL ..... FAILED"
-     exit 1
+    
 fi
