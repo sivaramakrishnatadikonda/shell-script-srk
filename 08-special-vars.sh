6d37c7a8b2af883f27f49a73305d7ab617e5f35d
sivaramakrishnatadikonda <sivaramakrishnatadikonda123@gmail.com>
2025-05-19 17:47:46 +0530

    hello world started

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 echo "All the variable pass the script: $@"
+echo " script name: $0"
+echo "pwd : $PWD"
+echo "user name : $USER"
+echo "number of varaibles pass the script: $#"
+echo "home directory: $HOME"
+echo "pid: $$"
+echo "backgroud pid: $!"
