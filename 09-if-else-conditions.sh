1d46fea22d8c838c1a9eaa962cef5f6f85615d1a
sivaramakrishnatadikonda <sivaramakrishnatadikonda123@gmail.com>
2025-05-19 18:20:57 +0530

    hello world started

--- 09-if-else-conditions.sh
+++ 09-if-else-conditions.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 NUMBER=$1
-if [ $NUMBER - lt 100 ]
+if [ $NUMBER - lt 10 ]
 
 then
 
-    echo "Give number $NUMBER is less than 100"
+    echo "Give number $NUMBER is less than 10"
     
 else
-         echo "Given number $NUMBER is not less than 100"
+         echo "Given number $NUMBER is not less than 10"
 fi
