4b8902b2747904611af98750fc38a0c0c1c12551
sivaramakrishnatadikonda <sivaramakrishnatadikonda123@gmail.com>
2025-05-19 18:37:03 +0530

    hello world started

--- 10-install-commands.sh
+++ 10-install-commands.sh
@@ -4,8 +4,11 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
      echo "ERROR: code running with root access"
+     exit 1
 else
     echo "you are running with root access"
 fi
+
+
 
 dnf install mysql -y
