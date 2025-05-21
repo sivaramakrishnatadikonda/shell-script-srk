a19d898cb25280fe7ac8f7d72d7ad442b5535a57
sivaramakrishnatadikonda <sivaramakrishnatadikonda123@gmail.com>
2025-05-21 15:31:00 +0530

    datatype data adding

--- 13-logs.sh
+++ 13-logs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 USERID=$(id -u)
-R=\e[31m
-G=\e[32m
-Y=\e[33m
-N=\e[33m
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
