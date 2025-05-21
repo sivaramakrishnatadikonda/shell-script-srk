b8e3a4f83263bcd8bb6711b75374093810f0aa22
sivaramakrishnatadikonda <sivaramakrishnatadikonda123@gmail.com>
2025-05-21 10:38:22 +0530

    datatype data adding

--- 06-datatype.sh
+++ 06-datatype.sh
@@ -1,10 +1,10 @@
 number1=100
 number2=200
 sum=$(($number1+$number2))
-echo "print sum data: $sum "
+echo "PRINT SUM DATA IS : $sum "
 
 NUMBER3=2
 NUMBER4=3
 ADDITION=$(($NUMBER3*$NUMBER4))
-echo "print ADDITIN DATA :$ADDITION"
+echo "PRINT ADDITIN DATA IS :$ADDITION"
 
