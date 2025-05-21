b5b03970d35d489ba89e28e54a5f8af7ddeab826
sivaramakrishnatadikonda <sivaramakrishnatadikonda123@gmail.com>
2025-05-21 10:48:34 +0530

    datatype data adding

--- 07-array.sh
+++ 07-array.sh
@@ -4,8 +4,8 @@ MOVIES=("HIT3" "#SINGLE" "PUSHPA2" "HAI" "MAD2")
 
 echo "First movie name:  ${MOVIES[0]}"
 echo "second movie name:  ${MOVIES[1]}"
-echo "3rd movie name:  ${MOVIES[1]}"
-echo "4th movie name:  ${MOVIES[1]}"
-echo "5th movie name:  ${MOVIES[1]}"
+echo "3rd movie name:  ${MOVIES[2]}"
+echo "4th movie name:  ${MOVIES[3]}"
+echo "5th movie name:  ${MOVIES[5]}"
 echo " All Movie names: ${MOVIES[@]}"
 echo " All Movie names: ${MOVIES[*]}"
