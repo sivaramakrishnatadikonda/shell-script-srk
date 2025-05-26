f07a7814fa0889266980993a79b09880b5cd7b77
sivaramakrishnatadikonda <sivaramakrishnatadikonda123@gmail.com>
2025-05-26 15:50:53 +0530

    miniproject

--- 16-miniproject.sh
+++ 16-miniproject.sh
@@ -2,9 +2,10 @@
 
 USERID=$(id -u)
 
-if [$USERID -ne 0 ]
+if [ $USERID -ne 0 ]
 then
     echo "ERROR:: please code running the root user"
+    exit 1
 else
     echo "your are code running the root user"
 
