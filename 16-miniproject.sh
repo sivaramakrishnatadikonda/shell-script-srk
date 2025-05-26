9281a4479a1747779b11d2f657534ef8bb85a3a9
sivaramakrishnatadikonda <sivaramakrishnatadikonda123@gmail.com>
2025-05-26 15:58:21 +0530

    miniproject

--- 16-miniproject.sh
+++ 16-miniproject.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 USERID=$(id -u)
+R=\e[31m
+G=\e[32m
+Y=\e[33m
+N=\e[0m
 
 if [ $USERID -ne 0 ]
 then
-    echo "ERROR:: please code running the root user"
+    echo "$R ERROR:: please code running the root user $N"
     exit 1
 else
     echo "your are code running the root user"
@@ -15,9 +19,9 @@ dnf install mysql -y
 if [ $? -eq 0 ]
 
 then
-    echo "Installation of mysql ------Sucessfully"
+    echo "Installation of mysql ------$G Sucessfully $N"
 
 else
-    echo "Installation of mysql ------Failed"
+    echo "Installation of mysql ------ $R Failed $N"
     exit 1
 fi
