664d7e66c979e311251cab30a5e27b8e9e51e595
sivaramakrishnatadikonda <sivaramakrishnatadikonda123@gmail.com>
2025-05-26 16:58:46 +0530

    miniproject

--- 16-miniproject.sh
+++ 16-miniproject.sh
@@ -1,46 +1,47 @@
 #!/bin/bash
 
 USERID=$(id -u)
-
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-LOGS_FOLDER="/var/log/miniproject-logs"
+
+LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME .log"
-PACKAGES=("mysql" "nodejs" "nginx" )
-mkdir -p $LOGS_FOLDER  
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+PACKAGES=("mysql" "nginx" "python")
+
+mkdir -p $LOGS_FOLDER
 echo "Script started executing at : $(date)" | tee -a $LOG_FILE
+
 if [ $USERID -ne 0 ]
 then
-     echo -e " $R ERROR: code running with root access $N " 
+     echo -e " $R ERROR: code running with root access $N " | tee -a $LOG_FILE
      exit 1
 else
-    echo -e " $G you are running with root access $N " 
+    echo -e " $G you are running with root access $N " | tee -a $LOG_FILE
 fi
 
 VALIDATE(){
 if [ $1 -eq 0 ]
 then
-     echo -e "Installing $2 is $G success $N " 
+     echo -e "Installing $2 is $G success $N " | tee -a $LOG_FILE
  else
-      echo -e "Installing $2 is $R failed $N " 
+      echo -e "Installing $2 is $R failed $N " | tee -a $LOG_FILE
       exit 1
 fi 
 
 }
-
 for package in $@
-do 
-dnf list installed nodejs
-if [ $? -eq 0 ]
-echo " $package is not installed please installed" | tee -a $LOG_FILE
-dnf install nodejs -y&>>LOG_FILE
+do
+   dnf list installed $package &>>LOG_FILE 
+   if [ $? -ne 0 ]
+then 
+   echo "$package is not installed please installed" | tee -a $LOG_FILE
+dnf install $package -y&>>$LOG_FILE
 VALIDATE $? "$package"
- else 
+
+else 
    echo -e " $Y $package is already installed....don't do anything $N " | tee -a $LOG_FILE
 fi
 done
-
-
