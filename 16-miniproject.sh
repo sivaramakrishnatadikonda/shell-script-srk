7ef1cfc20ce395636a8cb97eed5391a09010c0c0
sivaramakrishnatadikonda <sivaramakrishnatadikonda123@gmail.com>
2025-05-26 16:46:52 +0530

    miniproject

--- 16-miniproject.sh
+++ 16-miniproject.sh
@@ -6,7 +6,10 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-
+LOGS_FOLDER="/var/log/miniproject-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME .log"
+mkdir -p $LOGS_FOLDER  
 if [ $USERID -ne 0 ]
 then
      echo -e " $R ERROR: code running with root access $N " 
@@ -26,16 +29,16 @@ fi
 
 }
 
-dnf list installed nodejs
+dnf list installed nodejs &>>LOG_FILE
 
 if [ $? -ne 0 ]
 then 
-   echo "Nodejs is not installed please installed" 
-dnf install nodejs -y
+   echo "Nodejs is not installed please installed" | tee -a $LOG_FILE
+dnf install nodejs -y&>>LOG_FILE
 VALIDATE $? "Nodejs"
 
 else 
-   echo -e " $Y Nodejs is already installed....don't do anything $N "
+   echo -e " $Y Nodejs is already installed....don't do anything $N " | tee -a $LOG_FILE
 fi
 
 
