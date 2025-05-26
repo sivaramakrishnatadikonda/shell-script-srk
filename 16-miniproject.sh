71eabef456f76f5f131bed06f5503716151f39b5
sivaramakrishnatadikonda <sivaramakrishnatadikonda123@gmail.com>
2025-05-26 17:49:10 +0530

    miniproject

--- 16-miniproject.sh
+++ 16-miniproject.sh
@@ -1,47 +1,48 @@
-#!/bin/bash
+#?/bin/bash
 
 USERID=$(id -u)
+LOG_FOLDER="/var/log/miniporject-logs"
+SCRIPT_NAME=$ (echo $0 | cut -d "." -f1)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+PACKAGE=( "mysql" "nodejs" "python3" )
+
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOGS_FOLDER="/var/log/shellscript-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-PACKAGES=("mysql" "nginx" "python")
+mkdir -p $LOG_FOLDER
 
-mkdir -p $LOGS_FOLDER
-echo "Script started executing at : $(date)" | tee -a $LOG_FILE
 
-if [ $USERID -ne 0 ]
-then
-     echo -e " $R ERROR: code running with root access $N " | tee -a $LOG_FILE
-     exit 1
-else
-    echo -e " $G you are running with root access $N " | tee -a $LOG_FILE
+if [ $USERID -nq 0 ]
+then 
+  echo " $R ERROR :: $N  please run the script in root access"
+  exit 1
+else 
+  echo "your are run the script in root access"
 fi
 
 VALIDATE(){
+
 if [ $1 -eq 0 ]
-then
-     echo -e "Installing $2 is $G success $N " | tee -a $LOG_FILE
- else
-      echo -e "Installing $2 is $R failed $N " | tee -a $LOG_FILE
-      exit 1
-fi 
-
-}
-for package in $@
-do
-   dnf list installed $package &>>LOG_FILE 
-   if [ $? -ne 0 ]
 then 
-   echo "$package is not installed please installed" | tee -a $LOG_FILE
-dnf install $package -y&>>$LOG_FILE
-VALIDATE $? "$package"
-
+ echo "INSTALLING $2 IS  ------ $G SUCESSFULLY $N" | tee -a $LOG_FILE
 else 
-   echo -e " $Y $package is already installed....don't do anything $N " | tee -a $LOG_FILE
-fi
+    echo "INSTALLING $2 IS  ----- $R FAILED $N " | tee -a $LOG_FILE
+    exit 1
+fi 
+}
+
+for packages in $@&>>LOG_FILE
+do 
+dnf module list nodejs&>> LOG_FILE
+
+if [ $? -eq 0 ]
+then 
+  echo " $packages IS NOT INSTALLED PLEASE INSTALLED" | tee -a $LOG_FILE
+
+VALIDATE $? "$packages"
+else 
+   echo " $packages IS ALREADY INSTALLED NOTHING TO DO" | tee -a $LOG_FILE
+fi 
 done
